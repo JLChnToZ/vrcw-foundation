3fa532841c16c3d3a780055ca8a49bbfd6a65aa2
JLChnToZ <jeremylam619@gmail.com>
2025-11-24 00:15:10 +0800

    Tidy up shader inspector

--- Packages/idv.jlchntoz.vrcw-foundation/Editor/InspectorDrawers/UIModifiedInspector.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Editor/InspectorDrawers/UIModifiedInspector.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEditor;
+using JLChnToZ.VRC.Foundation.Editors;
 using JLChnToZ.VRC.Foundation.I18N;
 using JLChnToZ.VRC.Foundation.I18N.Editors;
 
 namespace JLChnToZ.VRCW.Foundation.Editor {
     public class UIModifiedInspector : ShaderGUI {
-        private EditorI18N locale;
+        static readonly string[] sdfModes = new string[3];
+        static readonly string[] specialRenderModes = new string[4];
+        EditorI18N locale;
 
         MaterialEditor editor;
         MaterialProperty[] properties;
@@ -30,21 +33,22 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
             EditorGUILayout.Space();
 
             DrawExperimentalSettings();
-            EditorGUILayout.Space();
 
             // Other Settings
-            FindAndDrawProperty("_UseUIAlphaClip", Loc("UIModified.UseAlphaClip", "Use Alpha Clip"));
-            FindAndDrawProperty("_Cull", Loc("UIModified.CullMode", "Cull Mode"));
-            FindAndDrawProperty("_ColorMask", Loc("UIModified.ColorMask", "Color Mask"));
-            FindAndDrawProperty("_StencilComp", Loc("UIModified.Stencil.Comp", "Comparison Mode"));
-            FindAndDrawProperty("_Stencil", Loc("UIModified.Stencil.ID", "ID"));
-            FindAndDrawProperty("_StencilOp", Loc("UIModified.Stencil.Operation", "Operation"));
-            FindAndDrawProperty("_StencilWriteMask", Loc("UIModified.Stencil.WriteMask", "Write Mask"));
-            FindAndDrawProperty("_StencilReadMask", Loc("UIModified.Stencil.ReadMask", "Read Mask"));
-            FindAndDrawProperty("unity_GUIZTestMode", Loc("UIModified.ZTestMode", "Z Test Mode"));
-            FindAndDrawProperty("_ZWrite", Loc("UIModified.ZWrite", "Z Write"));
+            FindAndDrawProperty("_UseUIAlphaClip", "UIModified.UseAlphaClip");
+            FindAndDrawProperty("_Cull", "UIModified.CullMode");
+            FindAndDrawProperty("_ColorMask", "UIModified.ColorMask");
+            FindAndDrawProperty("_StencilComp", "UIModified.Stencil.Comp");
+            FindAndDrawProperty("_Stencil", "UIModified.Stencil.ID");
+            FindAndDrawProperty("_StencilOp", "UIModified.Stencil.Operation");
+            FindAndDrawProperty("_StencilWriteMask", "UIModified.Stencil.WriteMask");
+            FindAndDrawProperty("_StencilReadMask", "UIModified.Stencil.ReadMask");
+            FindAndDrawProperty("unity_GUIZTestMode", "UIModified.ZTestMode");
+            FindAndDrawProperty("_ZWrite", "UIModified.ZWrite");
+
             editor.RenderQueueField();
             editor.EnableInstancingField();
+            editor.DoubleSidedGIField();
 
             if (changed.changed) {
                 // Update keywords for all target materials
@@ -60,14 +64,14 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
             var mainTex = FindProperty("_MainTex");
             var color = FindProperty("_Color") ?? FindProperty("_FaceColor");
             if (mainTex != null && color != null)
-                editor.TexturePropertySingleLine(new GUIContent(Loc("UIModified.SpriteAndColor", "Sprite and Color")), mainTex, color);
+                editor.TexturePropertySingleLine(locale.GetLocalizedContent("UIModified.SpriteAndColor"), mainTex, color);
             if (FindAndDrawProperty("_TextureWidth") |
                 FindAndDrawProperty("_TextureHeight") |
                 FindAndDrawProperty("_GradientScale") |
                 FindAndDrawProperty("_WeightNormal") |
                 FindAndDrawProperty("_WeightBold") |
                 FindAndDrawProperty("_ScaleRatioA")) {
-                EditorGUILayout.HelpBox(Loc("UIModified.Help.FontSettings", "These properties are controlled by TextMeshPro and should not be edited manually."), MessageType.Info);
+                EditorGUILayout.HelpBox(locale["UIModified.Help.FontSettings"], MessageType.Info);
             }
         }
 
@@ -75,53 +79,48 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
             var useSdf = FindProperty("_UseSDF");
             var useMsdf = FindProperty("_UseMSDF");
             if (useSdf == null && useMsdf == null) return;
-            EditorGUILayout.LabelField(Loc("UIModified.SDFSettings", "SDF Settings"), EditorStyles.boldLabel);
+            EditorGUILayout.LabelField(locale.GetLocalizedContent("UIModified.SDFSettings"), EditorStyles.boldLabel);
             int sdfMode = 0;
             if (IsPropertyOn(useSdf))
                 sdfMode = 1;
             else if (IsPropertyOn(useMsdf))
                 sdfMode = 2;
-            string[] sdfModes = new[] {
-                Loc("UIModified.SDFMode.NonSDF", "Non SDF"),
-                Loc("UIModified.SDFMode.SDF", "SDF"),
-                Loc("UIModified.SDFMode.MSDF", "MSDF")
-            };
-            int newSdfMode = EditorGUILayout.Popup(Loc("UIModified.TextureType", "Texture Type"), sdfMode, sdfModes);
+            sdfModes[0] = locale["UIModified.SDFMode.NonSDF"] ?? "";
+            sdfModes[1] = locale["UIModified.SDFMode.SDF"] ?? "";
+            sdfModes[2] = locale["UIModified.SDFMode.MSDF"] ?? "";
+            int newSdfMode = EditorGUILayout.Popup(locale.GetLocalizedContent("UIModified.TextureType"), sdfMode, sdfModes);
             if (newSdfMode != sdfMode) {
                 if (useSdf != null) useSdf.floatValue = (newSdfMode == 1) ? 1f : 0f;
                 if (useMsdf != null) useMsdf.floatValue = (newSdfMode == 2) ? 1f : 0f;
             }
             if (sdfMode != 0) {
                 var overrideMsdf = FindProperty("_OverrideMSDF");
-                if (overrideMsdf != null) editor.ShaderProperty(overrideMsdf, Loc("UIModified.UseOverrideTexture", "Use Override Texture"));
+                if (overrideMsdf != null) editor.ShaderProperty(overrideMsdf, locale.GetLocalizedContent("UIModified.UseOverrideTexture"));
                 if (overrideMsdf != null && overrideMsdf.floatValue > 0.5f) {
                     var msdfTex = FindProperty("_MSDFTex");
-                    if (msdfTex != null) editor.TexturePropertySingleLine(new GUIContent(Loc("UIModified.OverrideTexture", "Override Texture")), msdfTex);
+                    if (msdfTex != null) editor.TexturePropertySingleLine(locale.GetLocalizedContent("UIModified.OverrideTexture"), msdfTex);
                 }
-                FindAndDrawProperty("_PixelRange", Loc("UIModified.PixelRange", "Pixel Range"));
-                FindAndDrawProperty("_SDFThreshold", Loc("UIModified.Threshold", "Threshold"));
+                FindAndDrawProperty("_PixelRange", "UIModified.PixelRange");
+                FindAndDrawProperty("_SDFThreshold", "UIModified.Threshold");
             }
         }
 
         void DrawVRCSettings() {
-            EditorGUILayout.LabelField(Loc("UIModified.VRChatSettings", "VRChat Settings"), EditorStyles.boldLabel);
-            var vrcSupport = FindProperty("_VRCSupport");
-            bool isOn = false;
-            if (vrcSupport != null) {
-                MaterialEditor.BeginProperty(vrcSupport);
-                using (var check = new EditorGUI.ChangeCheckScope()) {
-                    isOn = IsPropertyOn(vrcSupport);
-                    isOn = EditorGUILayout.ToggleLeft(Loc("UIModified.ConditionalAppearance", "Conditional Appearance in Handheld Camera/Mirror"), isOn);
-                    if (check.changed) vrcSupport.floatValue = isOn ? 1f : 0f;
+            EditorGUILayout.LabelField(locale.GetLocalizedContent("UIModified.VRChatSettings"), EditorStyles.boldLabel);
+            var renderMode = FindProperty("_RenderMode");
+            if (renderMode != null) {
+                using var check = new EditorGUI.ChangeCheckScope();
+                editor.ShaderProperty(renderMode, locale.GetLocalizedContent("UIModified.VisibleModes"));
+                if (check.changed) {
+                    var vrcSupport = FindProperty("_VRCSupport");
+                    if (vrcSupport != null) vrcSupport.floatValue = (renderMode.intValue & 0xFFF) != 0xFFF ? 1f : 0f;
                 }
-                MaterialEditor.EndProperty();
             }
-            if (isOn) FindAndDrawProperty("_RenderMode", Loc("UIModified.VisibleModes", "Visible Modes"));
-            FindAndDrawProperty("_MirrorFlip", Loc("UIModified.FlipInMirror", "Flip in Mirror"));
+            FindAndDrawProperty("_MirrorFlip", "UIModified.FlipInMirror");
         }
 
         void DrawExperimentalSettings() {
-            EditorGUILayout.LabelField(Loc("UIModified.ExperimentalSettings", "Special Render Mode Settings (Experimental)"), EditorStyles.boldLabel);
+            EditorGUILayout.LabelField(locale.GetLocalizedContent("UIModified.ExperimentalSettings"), EditorStyles.boldLabel);
             var doubleSided = FindProperty("_DoubleSided");
             var billboard = FindProperty("_Billboard");
             var screenSpace = FindProperty("_ScreenSpaceOverlay");
@@ -132,24 +131,24 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
                 renderMode = 2;
             else if (IsPropertyOn(screenSpace))
                 renderMode = 3;
-            string[] specialRenderModes = new[] {
-                Loc("UIModified.RenderMode.Normal", "Normal"),
-                Loc("UIModified.RenderMode.DoubleSided", "Double Sided"),
-                Loc("UIModified.RenderMode.Billboard", "Billboard"),
-                Loc("UIModified.RenderMode.ScreenSpaceOverlay", "Screen Space Overlay")
-            };
-            var newRenderMode = EditorGUILayout.Popup(Loc("UIModified.RenderMode", "Render Mode"), renderMode, specialRenderModes);
-            if (newRenderMode != renderMode) {
-                if (doubleSided != null) doubleSided.floatValue = (newRenderMode == 1) ? 1f : 0f;
-                if (billboard != null) billboard.floatValue = (newRenderMode == 2) ? 1f : 0f;
-                if (screenSpace != null) screenSpace.floatValue = (newRenderMode == 3) ? 1f : 0f;
+            using (var check = new EditorGUI.ChangeCheckScope()) {
+                specialRenderModes[0] = locale["UIModified.RenderMode.Normal"] ?? "";
+                specialRenderModes[1] = locale["UIModified.RenderMode.DoubleSided"] ?? "";
+                specialRenderModes[2] = locale["UIModified.RenderMode.Billboard"] ?? "";
+                specialRenderModes[3] = locale["UIModified.RenderMode.ScreenSpaceOverlay"] ?? "";
+                renderMode = EditorGUILayout.Popup(locale.GetLocalizedContent("UIModified.RenderMode"), renderMode, specialRenderModes);
+                if (check.changed) {
+                    if (doubleSided != null) doubleSided.floatValue = (renderMode == 1) ? 1f : 0f;
+                    if (billboard != null) billboard.floatValue = (renderMode == 2) ? 1f : 0f;
+                    if (screenSpace != null) screenSpace.floatValue = (renderMode == 3) ? 1f : 0f;
+                }
             }
             switch (renderMode) {
                 case 1:
-                    EditorGUILayout.HelpBox(Loc("UIModified.Help.DoubleSided", "Double Sided mode requires geometry shader support. It don't work on mobile platforms."), MessageType.Info);
+                    EditorGUILayout.HelpBox(locale["UIModified.Help.DoubleSided"], MessageType.Info);
                     break;
                 case 2:
-                    EditorGUILayout.HelpBox(Loc("UIModified.Help.Billboard", "Billboard mode requires your canvas element has zero rotation in world space to work correctly."), MessageType.Info);
+                    EditorGUILayout.HelpBox(locale["UIModified.Help.Billboard"], MessageType.Info);
                     break;
                 case 3: {
                         var canvasRect = FindProperty("_CanvasRect");
@@ -158,7 +157,7 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
                             using (var check = new EditorGUI.ChangeCheckScope()) {
                                 var value = canvasRect.vectorValue;
                                 var rectValue = new Rect(value.x, value.y, value.z, value.w);
-                                rectValue = EditorGUILayout.RectField(Loc("UIModified.CanvasRect", "Canvas Rect"), rectValue);
+                                rectValue = EditorGUILayout.RectField(locale.GetLocalizedContent("UIModified.CanvasRect"), rectValue);
                                 if (check.changed) canvasRect.vectorValue = new Vector4(rectValue.x, rectValue.y, rectValue.width, rectValue.height);
                             }
                             MaterialEditor.EndProperty();
@@ -166,13 +165,13 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
                         var aspect = FindProperty("_AspectRatioMatch");
                         if (aspect != null) {
                             MaterialEditor.BeginProperty(aspect);
-                            EditorGUILayout.PrefixLabel(Loc("UIModified.AspectRatioMatch", "Aspect Ratio Match"));
+                            EditorGUILayout.PrefixLabel(locale.GetLocalizedContent("UIModified.AspectRatioMatch"));
                             using (new EditorGUILayout.HorizontalScope())
                             using (var check = new EditorGUI.ChangeCheckScope()) {
                                 var value = aspect.floatValue;
-                                GUILayout.Label(Loc("UIModified.WidthLabel", "W"));
+                                GUILayout.Label(locale.GetLocalizedContent("UIModified.WidthLabel"));
                                 value = EditorGUILayout.Slider(value, 0f, 1f, GUILayout.ExpandWidth(true));
-                                GUILayout.Label(Loc("UIModified.HeightLabel", "H"));
+                                GUILayout.Label(locale.GetLocalizedContent("UIModified.HeightLabel"));
                                 if (check.changed) aspect.floatValue = value;
                             }
                             MaterialEditor.EndProperty();
@@ -182,21 +181,17 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
             }
         }
 
-        MaterialProperty FindProperty(string name) {
-            try {
-                return FindProperty(name, properties);
-            } catch {
-                return null;
-            }
-        }
+        MaterialProperty FindProperty(string name) => FindProperty(name, properties, false);
 
         bool FindAndDrawProperty(string name, string displayName = null) {
             var prop = FindProperty(name);
-            if (prop != null) {
-                editor.ShaderProperty(prop, string.IsNullOrEmpty(displayName) ? prop.displayName : displayName);
-                return true;
-            }
-            return false;
+            if (prop == null) return false;
+            editor.ShaderProperty(prop,
+                string.IsNullOrEmpty(displayName) ?
+                    Utils.GetTempContent(prop.displayName) :
+                    locale.GetLocalizedContent(displayName)
+            );
+            return true;
         }
 
         static void UpdateKeywords(Material mat) {
@@ -220,16 +215,6 @@ namespace JLChnToZ.VRCW.Foundation.Editor {
             mat.DisableKeyword(keyword);
         }
 
-        static bool IsPropertyOn(MaterialProperty prop) {
-            return prop != null && prop.floatValue > 0.5f;
-        }
-
-        string Loc(string key, string fallback) {
-            if (locale != null) {
-                var v = locale[key];
-                if (!string.IsNullOrEmpty(v)) return v;
-            }
-            return fallback;
-        }
+        static bool IsPropertyOn(MaterialProperty prop) => prop != null && prop.floatValue > 0.5f;
     }
 }
