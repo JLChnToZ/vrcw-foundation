9433791a3635034f16387170003120a9915f1d7a
JLChnToZ <jeremylam619@gmail.com>
2026-05-08 02:16:47 +0800

    Add filtered udon event listener

--- Packages/idv.jlchntoz.vrcw-foundation/Editor/Preprocessors/BindUdonSharpEventPreprocessor.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Editor/Preprocessors/BindUdonSharpEventPreprocessor.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using VRC.SDK3.Data;
 using VRC.Udon;
 using UdonSharp;
 using UdonSharpEditor;
@@ -11,29 +14,33 @@ using UnityObject = UnityEngine.Object;
 
 namespace JLChnToZ.VRC.Foundation.Editors {
     internal sealed class BindUdonSharpEventPreprocessor : UdonSharpPreProcessor {
-        readonly Dictionary<UdonSharpEventSender, List<UnityObject>> eventSenders = new Dictionary<UdonSharpEventSender, List<UnityObject>>();
+        readonly Dictionary<UdonSharpEventSender, HashSet<UnityObject>> eventSenders =
+            new Dictionary<UdonSharpEventSender, HashSet<UnityObject>>();
+        readonly Dictionary<UdonSharpEventSender, HashSet<(string eventName, UnityObject target)>> eventSenderWithEventNames =
+            new Dictionary<UdonSharpEventSender, HashSet<(string, UnityObject)>>();
 
         protected override void ProcessEntry(Type type, MonoBehaviour dest, UdonBehaviour udon) {
             var fieldInfos = GetFields<BindUdonSharpEventAttribute>(type);
             foreach (var field in fieldInfos) {
                 var targetObj = field.GetValue(dest);
+                var attr = field.GetCustomAttribute<BindUdonSharpEventAttribute>(true);
                 if (targetObj is Array array)
                     for (int i = 0, length = array.GetLength(0); i < length; i++)
-                        AddEntry(array.GetValue(i) as UnityObject, dest);
+                        AddEntry(array.GetValue(i) as UnityObject, dest, attr.bindEventNames);
                 else if (targetObj is UnityObject unityObject)
-                    AddEntry(unityObject, dest);
+                    AddEntry(unityObject, dest, attr.bindEventNames);
             }
         }
 
         public override void OnPreprocess(Scene scene) {
             base.OnPreprocess(scene);
-            var remapped = new HashSet<UnityObject>();
             foreach (var kv in eventSenders) {
                 var sender = kv.Key;
                 if (sender == null) {
                     Debug.LogError("[BindUdonSharpEventPreprocessor] Event sender is null, this should not happen.", sender);
                     continue;
                 }
+                using (HashSetPool<UnityObject>.Get(out var remapped))
                 using (var so = new SerializedObject(sender)) {
                     var prop = so.FindProperty("targets");
                     for (int i = 0, count = prop.arraySize; i < count; i++)
@@ -46,22 +53,61 @@ namespace JLChnToZ.VRC.Foundation.Editors {
                         foreach (var entry in remapped)
                             prop.GetArrayElementAtIndex(i++).objectReferenceValue = entry;
                     }
-                    remapped.Clear();
                     so.ApplyModifiedPropertiesWithoutUndo();
                 }
-                UdonSharpEditorUtility.CopyProxyToUdon(sender);
             }
             eventSenders.Clear();
+            foreach (var kv in eventSenderWithEventNames) {
+                var sender = kv.Key;
+                if (sender == null) {
+                    Debug.LogError("[BindUdonSharpEventPreprocessor] Event sender is null, this should not happen.", sender);
+                    continue;
+                }
+                var eventTargetMap = sender.namedTargets ??= new DataDictionary();
+                foreach (var (eventName, target) in kv.Value) {
+                    if (string.IsNullOrEmpty(eventName)) continue;
+                    if (target == null) continue;
+                    if (!eventTargetMap.TryGetValue(eventName, out var dt)) {
+                        eventTargetMap[eventName] = target;
+                        continue;
+                    }
+                    DataList dtList;
+                    switch (dt.TokenType) {
+                        case TokenType.Reference:
+                            if (dt == target) break;
+                            eventTargetMap[eventName] = new DataList { dt, target };;
+                            break;
+                        case TokenType.DataList:
+                            dtList = dt.DataList;
+                            if (dtList.Contains(target)) break;
+                            dtList.Add(target);
+                            break;
+                    }
+                }
+            }
+            using (HashSetPool<UdonSharpEventSender>.Get(out var proceedSenders)) {
+                proceedSenders.UnionWith(eventSenders.Keys);
+                proceedSenders.UnionWith(eventSenderWithEventNames.Keys);
+                foreach (var sender in proceedSenders)
+                    UdonSharpEditorUtility.CopyProxyToUdon(sender);
+            }
         }
 
-        void AddEntry(UnityObject targetObj, UnityObject dest) {
+        void AddEntry(UnityObject targetObj, UnityObject dest, string[] eventNames) {
             if (targetObj == null) return;
             if (targetObj is GameObject go) targetObj = go.GetComponent<UdonSharpEventSender>();
             else if (targetObj is UdonBehaviour ub) targetObj = UdonSharpEditorUtility.GetProxyBehaviour(ub);
             if (!(targetObj is UdonSharpEventSender sender) || sender == null) return;
-            if (!eventSenders.TryGetValue(sender, out var list))
-                eventSenders[sender] = list = new List<UnityObject>();
-            list.Add(dest);
+            if (eventNames != null && eventNames.Length > 0) {
+                if (!eventSenderWithEventNames.TryGetValue(sender, out var list))
+                    eventSenderWithEventNames[sender] = list = new HashSet<(string, UnityObject)>();
+                foreach (var eventName in eventNames)
+                    list.Add((eventName, dest));
+            } else {
+                if (!eventSenders.TryGetValue(sender, out var list))
+                    eventSenders[sender] = list = new HashSet<UnityObject>();
+                list.Add(dest);
+            }
         }
     }
 }
--- Packages/idv.jlchntoz.vrcw-foundation/Runtime/UdonSharpEventSender.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Runtime/UdonSharpEventSender.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using VRC.SDKBase;
 using UdonSharp;
+using VRC.SDK3.Data;
+using VRC.Udon;
 
 namespace JLChnToZ.VRC.Foundation {
     /// <summary>
@@ -10,8 +12,23 @@ namespace JLChnToZ.VRC.Foundation {
     /// <remarks>
     /// This is equivalent to calling <see cref="UdonSharpEventSender._AddListener"/> in runtime but done in build time.
     /// </remarks>
-    [AttributeUsage(AttributeTargets.Field)]
-    public class BindUdonSharpEventAttribute : Attribute {}
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class BindUdonSharpEventAttribute : Attribute {
+        internal readonly string[] bindEventNames;
+
+        /// <summary>
+        /// Bind the event sender to the target field.
+        /// </summary>
+        /// <param name="bindEventNames">
+        /// The names of the events to bind.
+        /// If no event names are provided, all events will be bound.<br/>
+        /// Recommended to provide event names when you have many events but only a few of them need to be listened,
+        /// as this can reduce the number of events sent and improve performance.
+        /// </param>
+        public BindUdonSharpEventAttribute(params string[] bindEventNames) {
+            this.bindEventNames = bindEventNames;
+        }
+    }
 
     public abstract class UdonSharpEventSender : UdonSharpBehaviour {
         /// <summary>
@@ -22,6 +39,7 @@ namespace JLChnToZ.VRC.Foundation {
         /// Whether to log the events sent.
         /// </summary>
         [SerializeField] protected bool logEvents = false;
+        [HideInInspector] public DataDictionary namedTargets;
 
         /// <summary>
         /// Add a listener to the event.
@@ -41,12 +59,61 @@ namespace JLChnToZ.VRC.Foundation {
         }
 
         /// <summary>
+        /// Add a listener to the event with a specific event name.
+        /// The event will be sent when <see cref="SendEvent(string)"/> is called with the same event name.
+        /// </summary>
+        /// <param name="callback">The callback listener.</param>
+        /// <param name="eventName">The event name.</param>
+        /// <remarks>
+        /// This can reduces the number of event sent and improve performance when you have many events
+        /// but only a few of them need to be listened, as the event will only be sent to the listeners that are interested in the event.
+        /// </remarks>
+        public void _AddListener(UdonSharpBehaviour callback, string eventName) {
+            if (!Utilities.IsValid(callback)) return;
+            if (!Utilities.IsValid(namedTargets)) namedTargets = new DataDictionary();
+            DataToken eventNameToken = eventName;
+            if (!namedTargets.TryGetValue(eventNameToken, out var dt)) {
+                namedTargets[eventNameToken] = callback;
+                return;
+            }
+            DataList dtList;
+            switch (dt.TokenType) {
+                case TokenType.Reference:
+                    if (dt == callback) return;
+                    dtList = new DataList();
+                    dtList.Add(dt);
+                    dtList.Add(callback);
+                    namedTargets[eventNameToken] = dtList;
+                    return;
+                case TokenType.DataList:
+                    dtList = dt.DataList;
+                    if (dtList.Contains(callback)) return;
+                    dtList.Add(callback);
+                    return;
+            }
+        }
+
+        /// <summary>
         /// Send an event to all targets.
         /// </summary>
         /// <param name="name">The event name.</param>
         protected void SendEvent(string name) {
             if (!Utilities.IsValid(targets)) return;
             if (logEvents) Debug.Log($"[{GetUdonTypeName()}] Send Event {name}");
+            if (Utilities.IsValid(namedTargets) && namedTargets.TryGetValue(name, out var dt))
+                switch (dt.TokenType) {
+                    case TokenType.Reference:
+                        var target = (UdonBehaviour)dt.Reference;
+                        if (Utilities.IsValid(target)) target.SendCustomEvent(name);
+                        break;
+                    case TokenType.DataList:
+                        var dtList = dt.DataList;
+                        for (int i = 0, count = dtList.Count; i < count; i++) {
+                            var targetI = (UdonBehaviour)dtList[i].Reference;
+                            if (Utilities.IsValid(targetI)) targetI.SendCustomEvent(name);
+                        }
+                        break;
+                }
             foreach (var ub in targets) if (Utilities.IsValid(ub)) ub.SendCustomEvent(name);
         }
 
