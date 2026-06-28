4f18323e72540c92d26f3fa020f5c385bc071a48
JLChnToZ <jeremylam619@gmail.com>
2026-06-28 16:47:22 +0800

    Implement attribute to undefine a declare preprocessor

--- Packages/idv.jlchntoz.vrcw-foundation/Editor/DefineManager.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Editor/DefineManager.cs
@@ -1,41 +1,68 @@
 using System;
+using System.Reflection;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 
 namespace JLChnToZ.VRC.Foundation.Editors {
     static class DefineManager {
+        static readonly StringComparer defineComparer = StringComparer.Ordinal;
+
         [InitializeOnLoadMethod]
         static void OnLoad() {
             var appDomain = AppDomain.CurrentDomain;
             var assemblies = appDomain.GetAssemblies();
-            var attributes = new HashSet<DeclareDefineAttribute>();
-            foreach (var assembly in assemblies) {
-                var assemblyAttributes = assembly.GetCustomAttributes(typeof(DeclareDefineAttribute), false);
-                if (assemblyAttributes.Length > 0) attributes.UnionWith(assemblyAttributes.Cast<DeclareDefineAttribute>());
-            }
-            if (attributes.Count > 0) DeclareDefines(attributes);
+            var declares = new HashSet<DeclareInfo>();
+            foreach (var assembly in assemblies) GetDeclareInfosFromAssembly(declares, assembly);
+            if (declares.Count > 0) DeclareDefines(declares);
             appDomain.AssemblyLoad += OnAssemblyLoad;
         }
 
-        static void OnAssemblyLoad(object sender, AssemblyLoadEventArgs args) {
-            var assembly = args.LoadedAssembly.GetCustomAttributes(typeof(DeclareDefineAttribute), false);
-            if (assembly.Length == 0) return;
-            var attributes = assembly.Cast<DeclareDefineAttribute>();
-            EditorApplication.delayCall += () => DeclareDefines(attributes);
+        static void GetDeclareInfosFromAssembly(HashSet<DeclareInfo> results, Assembly assembly) {
+            var assemblyAttributes = assembly.GetCustomAttributes(typeof(DeclareDefineBaseAttribute), true);
+            foreach (var o in assemblyAttributes)
+                if (o is DeclareDefineAttribute declareAttr)
+                    results.Add(new DeclareInfo(declareAttr.DefineName, true));
+                else if (o is DeclareUndefineAttribute undefineAttr)
+                    results.Add(new DeclareInfo(undefineAttr.DefineName, false));
         }
 
-        static void DeclareDefines(IEnumerable<DeclareDefineAttribute> attributes) {
+        static void OnAssemblyLoad(object sender, AssemblyLoadEventArgs args) {
+            var declares = new HashSet<DeclareInfo>();
+            GetDeclareInfosFromAssembly(declares, args.LoadedAssembly);
+            if (declares.Count == 0) return;
+            EditorApplication.delayCall += () => DeclareDefines(declares);
+        }
+
+        static void DeclareDefines(IEnumerable<DeclareInfo> infos) {
             var target = EditorUserBuildSettings.activeBuildTarget;
             var group = BuildPipeline.GetBuildTargetGroup(target);
             var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
-            var defines = new HashSet<string>(definesString.Split(';', StringSplitOptions.RemoveEmptyEntries));
+            var defines = new HashSet<string>(definesString.Split(';', StringSplitOptions.RemoveEmptyEntries), defineComparer);
             bool changed = false;
-            foreach (var attr in attributes)
-                if (!string.IsNullOrWhiteSpace(attr.DefineName) && defines.Add(attr.DefineName))
-                    changed = true;
+            foreach (var info in infos)
+                changed |= info.isDefine ? defines.Add(info.defineName) : defines.Remove(info.defineName);
             if (!changed) return;
             PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defines));
+        }
+
+        readonly struct DeclareInfo : IEquatable<DeclareInfo> {
+            public readonly string defineName;
+            public readonly bool isDefine;
+
+            public DeclareInfo(string defineName, bool isDefine) {
+                this.defineName = defineName;
+                this.isDefine = isDefine;
+            }
+
+            public bool Equals(DeclareInfo other) =>
+                defineComparer.Equals(defineName, other.defineName) &&
+                isDefine == other.isDefine;
+
+            public override bool Equals(object obj) => obj is DeclareInfo other && Equals(other);
+
+            public override int GetHashCode() => HashCode.Combine(defineComparer.GetHashCode(defineName), isDefine);
+
+            override public string ToString() => $"{(isDefine ? "Define" : "Undefine")} {defineName}";
         }
     }
 }
--- Packages/idv.jlchntoz.vrcw-foundation/Runtime/Attributes Markers/DeclareDefineAttribute.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Runtime/Attributes Markers/DeclareDefineAttribute.cs
@@ -8,13 +8,28 @@ namespace JLChnToZ.VRC.Foundation {
     /// This is the alternative to using "versionDefines" in manifest.json, since that don't work with UdonSharp compiler.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-    public class DeclareDefineAttribute : Attribute {
+    public sealed class DeclareDefineAttribute : DeclareDefineBaseAttribute {
+        public DeclareDefineAttribute(string defineName) : base(defineName) {
+        }
+    }
+
+    /// <summary>
+    /// Attach this to an assembly to remove a define.
+    /// This is for handling cases where a dependency has been removed.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class DeclareUndefineAttribute : DeclareDefineBaseAttribute {
+        public DeclareUndefineAttribute(string defineName) : base(defineName) {
+        }
+    }
+
+    public abstract class DeclareDefineBaseAttribute : Attribute {
         /// <summary>
-        /// The name of the define to declare.
+        /// The name of the define to declare or remove.
         /// </summary>
         public string DefineName { get; }
 
-        public DeclareDefineAttribute(string defineName) {
+        protected DeclareDefineBaseAttribute(string defineName) {
             DefineName = defineName;
         }
     }
