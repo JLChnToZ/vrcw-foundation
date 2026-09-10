f7fec9f081e991673129e71d39df5a6d520b8183
JLChnToZ <jeremylam619@gmail.com>
2026-09-11 00:28:28 +0800

    Fix nested resolution breaks even target value filled

--- Packages/idv.jlchntoz.vrcw-foundation/Editor/Preprocessors/ResolvePreprocessor.cs
+++ Packages/idv.jlchntoz.vrcw-foundation/Editor/Preprocessors/ResolvePreprocessor.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 using VRC.SDK3.Data;
@@ -14,30 +15,34 @@ using UnityObject = UnityEngine.Object;
 
 namespace JLChnToZ.VRC.Foundation.Editors {
     internal sealed class ResolvePreprocessor : UdonSharpPreProcessor {
-        static readonly Dictionary<FieldInfo, Resolver> resolveFields = new Dictionary<FieldInfo, Resolver>();
+        static readonly Dictionary<FieldInfo, (Resolver resolver, bool nullOnly)> resolveFields = new Dictionary<FieldInfo, (Resolver, bool)>();
         readonly Dictionary<(FieldInfo, UnityObject), object> resolvedObjects = new Dictionary<(FieldInfo, UnityObject), object>();
 
         public override int Priority => -1; // Earlier than binding preprocessor.
 
-        static Resolver ResolveField(FieldInfo field) {
-            if (!resolveFields.TryGetValue(field, out var resolver)) {
-                resolver = Resolver.Create();
-                bool hasAttribute = false;
+        static (Resolver resolver, bool nullOnly) GetResolver(FieldInfo field) {
+            if (!resolveFields.TryGetValue(field, out var entry)) {
+                entry = (Resolver.Create(), false);
+                bool hasAttribute = false, isFirst = false;
                 foreach (var attribute in field.GetCustomAttributes<ResolveAttribute>(true)) {
+                    if (!isFirst) {
+                        entry.nullOnly = attribute.NullOnly;
+                        isFirst = true;
+                    }
                     var srcType = attribute.SourceType;
                     if (srcType != null) {
                         var srcPath = attribute.Source;
                         int hashIndex = srcPath.LastIndexOf("#");
                         if (hashIndex < 0) {
-                            resolver.WithType(srcType);
-                            resolver.WithPath(srcPath);
+                            entry.resolver.WithType(srcType);
+                            entry.resolver.WithPath(srcPath);
                         } else {
-                            resolver.WithPath(srcPath.Substring(0, hashIndex));
-                            resolver.WithType(srcType);
-                            resolver.WithPath(srcPath.Substring(hashIndex + 1));
+                            entry.resolver.WithPath(srcPath.Substring(0, hashIndex));
+                            entry.resolver.WithType(srcType);
+                            entry.resolver.WithPath(srcPath.Substring(hashIndex + 1));
                         }
                     } else
-                        resolver.WithPath(attribute.Source);
+                        entry.resolver.WithPath(attribute.Source);
                     hasAttribute = true;
                 }
                 if (hasAttribute) {
@@ -46,12 +51,12 @@ namespace JLChnToZ.VRC.Foundation.Editors {
                     if (fieldType != typeof(DataToken) &&
                         fieldType != typeof(DataDictionary) &&
                         fieldType != typeof(DataList))
-                        resolver.WithType(fieldType);
+                        entry.resolver.WithType(fieldType);
                 } else
-                    resolver = null;
-                resolveFields[field] = resolver;
+                    entry.resolver = null;
+                resolveFields[field] = entry;
             }
-            return resolver;
+            return entry;
         }
 
         static bool IsObjectSafeToAdd(object obj) {
@@ -139,30 +144,34 @@ namespace JLChnToZ.VRC.Foundation.Editors {
                 result = null;
                 return false;
             }
-            var resolver = ResolveField(field);
+            var (resolver, nullOnly) = GetResolver(field);
             if (resolver != null) {
                 if (resolvedObjects.TryGetValue((field, instance), out result)) return true;
-                resolvedObjects[(field, instance)] = null; // Circular reference protection.
+                resolvedObjects[(field, instance)] = result = field.GetValue(instance);
                 var fieldType = field.FieldType;
                 if (fieldType.IsArray) {
+                    if (nullOnly && result is Array array && array.Length > 0)
+                        return true;
                     var elementType = fieldType.GetElementType();
-                    var results = new List<object>();
-                    foreach (var obj in resolver.Resolve(instance)) {
-                        if (CastHelper.TryCast(obj, elementType, out var casted) && IsObjectSafeToAdd(casted)) {
-                            results.Add(casted);
-                            continue;
+                    using (ListPool<object>.Get(out var results)) {
+                        foreach (var obj in resolver.Resolve(instance)) {
+                            if (CastHelper.TryCast(obj, elementType, out var casted) && IsObjectSafeToAdd(casted)) {
+                                results.Add(casted);
+                                continue;
+                            }
+                            if (obj is IEnumerable enumerable)
+                                foreach (var item in enumerable)
+                                    if (CastHelper.TryCast(item, elementType, out casted) && IsObjectSafeToAdd(casted))
+                                        results.Add(casted);
                         }
-                        if (obj is IEnumerable enumerable)
-                            foreach (var item in enumerable)
-                                if (CastHelper.TryCast(item, elementType, out casted) && IsObjectSafeToAdd(casted))
-                                    results.Add(casted);
+                        array = Array.CreateInstance(elementType, results.Count);
+                        for (int i = 0; i < results.Count; i++)
+                            array.SetValue(results[i], i);
                     }
-                    var array = Array.CreateInstance(elementType, results.Count);
-                    for (int i = 0; i < results.Count; i++)
-                        array.SetValue(results[i], i);
                     resolvedObjects[(field, instance)] = result = array;
                     return true;
                 }
+                if (nullOnly && result.IsValid()) return true;
                 if (resolver.TryResolve(instance, out result) && IsObjectSafeToAdd(result)) {
                     resolvedObjects[(field, instance)] = result;
                     return true;
